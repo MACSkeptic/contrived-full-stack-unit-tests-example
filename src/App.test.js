8a363618a3934446c7f3828d89ebbee594b2f6c8
mozair ♥ ☕ <macskeptic@gmail.com>
2018-09-14 08:43:24 +0200

    tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render, waitForElement, wait, cleanup } from 'react-testing-library';
+import { fireEvent, render, waitForElement, cleanup } from 'react-testing-library';
 import App from './App';
 import { MemoryRouter } from 'react-router';
 import { Provider } from 'react-redux';
@@ -37,8 +37,23 @@ const renderUsingRedux = () => (
   ['context', renderUsingContext]
 ].forEach(([framework, subject]) => {
   describe(`tests using ${framework}`, () => {
+    it('allows users to pick between context and redux', () => {
+      sandbox.stub(http, 'httpGetAnimals').callsFake(() => (new Promise(_.noop)));
+      const rendered = subject();
+      return Promise.resolve()
+        .then(() => waitForElement(() => rendered.getByText('context', { exact: false })))
+        .then(() => waitForElement(() => rendered.getByText('redux', { exact: false })));
+    });
+    it('allows users to pick between magical, real, and all', () => {
+      sandbox.stub(http, 'httpGetAnimals').callsFake(() => (new Promise(_.noop)));
+      const rendered = subject();
+      return Promise.resolve()
+        .then(() => waitForElement(() => rendered.getByText('magical', { exact: false })))
+        .then(() => waitForElement(() => rendered.getByText('real', { exact: false })))
+        .then(() => waitForElement(() => rendered.getByText('all', { exact: false })));
+    });
     it('filters when the menu is clicked', () => {
-      const stub = sandbox.stub(http, 'httpGetAnimals').callsFake((filter) => (
+      sandbox.stub(http, 'httpGetAnimals').callsFake((filter) => (
         (filter === 'magical') ? Promise.resolve([{
           emoji: '🐈',
           name: 'cat'
@@ -48,23 +63,45 @@ const renderUsingRedux = () => (
         }])
       ));
       const rendered = subject();
-      return wait(() => expect(stub.called).toEqual(true))
+      return Promise.resolve()
         .then(() => waitForElement(() => rendered.getByText('sheep', { exact: false })))
         .then(() => waitForElement(() => rendered.getByText('magical', { exact: false })))
         .then((link) => fireEvent.click(link))
         .then(() => waitForElement(() => rendered.getByText('cat', { exact: false })));
     });
-    it('renders a loading indicator', () => {
+    it('renders an error on navigation', () => {
+      _.tap(sandbox.stub(http, 'httpGetAnimals'), (stub) => ([
+        stub.onCall(0).resolves([]),
+        stub.onCall(1).rejects(new Error('onoes'))
+      ]));
+      const rendered = subject();
+      return Promise.resolve()
+        .then(() => waitForElement(() => rendered.getByText('magical', { exact: false })))
+        .then((link) => fireEvent.click(link))
+        .then(() => waitForElement(() => rendered.getByText('onoes', { exact: false })));
+    });
+    it('renders a loading indicator on navigation', () => {
+      _.tap(sandbox.stub(http, 'httpGetAnimals'), (stub) => ([
+        stub.onCall(0).resolves([]),
+        stub.onCall(1).returns(new Promise(_.noop))
+      ]));
+      const rendered = subject();
+      return Promise.resolve()
+        .then(() => waitForElement(() => rendered.getByText('magical', { exact: false })))
+        .then((link) => fireEvent.click(link))
+        .then(() => waitForElement(() => rendered.getByText('loading', { exact: false })));
+    });
+    it('renders a loading indicator on first load', () => {
       sandbox.stub(http, 'httpGetAnimals').callsFake(() => (new Promise(_.noop)));
       const rendered = subject();
       return waitForElement(() => rendered.getByText('loading', { exact: false }));
     });
-    it('renders an error', () => {
+    it('renders an error on first load', () => {
       sandbox.stub(http, 'httpGetAnimals').callsFake(() => Promise.reject(new Error('onoes')));
       const rendered = subject();
       return waitForElement(() => rendered.getByText('onoes', { exact: false }));
     });
-    it('renders a sheep', () => {
+    it('renders a sheep on first load', () => {
       sandbox.stub(http, 'httpGetAnimals').callsFake(() => (
         Promise.resolve([{
           emoji: '🐑',
